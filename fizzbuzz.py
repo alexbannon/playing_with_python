3267aee4f5b7dfb7a76ecbc860b7537a59487b78
Alex Bannon <alexbannon@gmail.com>
2015-09-08 13:11:17 -0400

    added fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+fizzbuzzlist = list(range(100))
+print(fizzbuzzlist)
+for number in fizzbuzzlist:
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
